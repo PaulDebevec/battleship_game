6ae46a78340cd304fea99b1a59b67c94fbab1476
Paul Debevec <paulmdebevec@gmail.com>
2019-10-07 19:28:21 -0600

    Added ShipTest class and test_it_exists method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,1 +1,14 @@
-require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def setup
+  end
+
+  def test_it_exists
+    ship = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, ship
+  end
+
+end
