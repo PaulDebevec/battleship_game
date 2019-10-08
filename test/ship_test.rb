1977f3197b856d0c11209cc3bc269f31ad20a167
Paul Debevec <paulmdebevec@gmail.com>
2019-10-07 19:53:40 -0600

    Added test_ship_health

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -16,4 +16,9 @@ class ShipTest < Minitest::Test
     assert_equal ship.length, 3
   end
 
+  def test_ship_health
+    ship = Ship.new("Cruiser", 3, 3)
+    assert_equal(3, ship.health)
+  end
+
 end
