115d3d670be71036fb5fd66131b2b198161546af
Paul Debevec <paulmdebevec@gmail.com>
2019-10-09 14:33:02 -0600

    Created Cell class and initialized

---
+++ lib/cell.rb
@@ -0,0 +1,21 @@
+class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = []
+  end
+
+  def empty?
+    @ship == []
+  end
+
+  def place_ship(boats)
+    @ship << boats
+    @ship[0]
+  end
+
+  def ship
+
+  end
+end
