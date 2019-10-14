b181f90478ff4fd4f34c769b1ac181c302fd2acf
Paul Debevec <paulmdebevec@gmail.com>
2019-10-14 07:41:39 -0600

    Added succession? helper method and completed valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -22,8 +22,29 @@ class Board
     @cells.key?(coord)
   end
 
+ def succession?(array_vars)
+    return array_vars.each_cons(2).all? {|co| co[1] == co[0].succ}
+ end
+
   def valid_placement?(ship, coords)
-    ship.length == coords.length
+    return false if ship.length != coords.length
+
+    rows = coords.map {|coord| coord[0]}
+    columns = coords.map {|coord| coord[1]}
+
+    row_in_succession = succession?(rows)
+    same_row = rows.uniq.length == 1
+
+    column_in_succession = succession?(columns)
+    same_column = columns.uniq.length == 1
+
+    if row_in_succession && same_column
+      true
+    elsif column_in_succession && same_row
+      true
+    else
+      false
+    end
   end
 
 end
