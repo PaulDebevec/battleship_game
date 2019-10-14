a2bd339e9e9c7981d0df31f9c43b4070ef6ce595
Paul Debevec <paulmdebevec@gmail.com>
2019-10-14 07:48:17 -0600

    Completed tests for the board class valid_placement? method

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,6 +8,8 @@ require 'pry'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_does_the_board_exist
@@ -54,15 +56,53 @@ class BoardTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_is_the_ship_on_a_valid_placement
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
+  def test_is_the_ship_on_a_valid_horizontal_placement
     expected = false
-    actual = @board.valid_placement?(cruiser, %w[A1 A2])
+    actual = @board.valid_placement?(@cruiser, %w[A1 A2])
     assert_equal expected, actual
 
-    actual = @board.valid_placement?(submarine, %w[A2 A3 A4])
+    actual = @board.valid_placement?(@submarine, %w[A2 A3 A4])
+    assert_equal expected, actual
+
+    expected = true
+    actual = @board.valid_placement?(@cruiser, %w[B1 B2 B3])
+    assert_equal expected, actual
+
+    actual = @board.valid_placement?(@submarine, %w[C2 C3])
     assert_equal expected, actual
   end
 
+  def test_is_ship_on_a_valid_vertical_placement
+    expected = true
+    actual = @board.valid_placement?(@cruiser, %w[A1 B1 C1])
+    assert_equal expected, actual
+
+    actual = @board.valid_placement?(@submarine, %w[B3 C3])
+    assert_equal expected, actual
+
+    expected = false
+    actual = @board.valid_placement?(@cruiser, %w[A1 B4 C2])
+    assert_equal expected, actual
+
+    actual = @board.valid_placement?(@submarine, %w[B3 C1])
+    assert_equal expected, actual
+  end
+
+  def test_ships_cannot_be_placed_diagonally
+    expected = false
+    actual = @board.valid_placement?(@submarine, %w[A1 B2])
+    assert_equal expected, actual
+
+    actual = @board.valid_placement?(@cruiser, %w[A1 B2 C3])
+    assert_equal expected, actual
+  end
+
+  def test_place_ship_places_ship
+    expected = true
+    actual = @board.place(@cruiser, ["A1", "A2", "A3"])
+    assert_equal expected, actual
+
+    actual = @board.place(@submarine, ["C3", "D3"])
+    assert_equal expected, actual
+  end
 end
