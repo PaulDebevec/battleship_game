d44538e67b3389110a219648b719ef2bb931877b
Paul Debevec <paulmdebevec@gmail.com>
2019-10-15 14:57:56 -0600

    Added render method
    Co-Authored-By: Phil DeLong <philjdelong@gmail.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
+require './lib/cell'
 
 class Board
     attr_reader :cells
+    attr_accessor :render
 
   def initialize
     @cells = {}
@@ -31,10 +33,12 @@ class Board
     succession?(rows) && same?(columns) || succession?(columns) && same?(rows)
   end
 
+# Helper method for valid_placement?
   def same?(array_to_check)
     return array_to_check.uniq.length == 1
   end
 
+# Helper method for valid_placement?
   def succession?(array_vars)
      return array_vars.each_cons(2).all? {|co| co[1] == co[0].succ}
   end
@@ -46,9 +50,16 @@ class Board
     end
   end
 
+# Helper method for place
   def no_ship_in_cell(coords)
     coords.all? do |coord|
       @cells[coord].empty?
     end
+  end
+
+  def render(show_cell = false)
+    @cells.values.map { |cell| "#{cell.render(show_cell)}" }
+
+    "  1 2 3 4 \nA #{@cells.values[0].render} #{@cells.values[1].render} #{@cells.values[2].render} #{@cells.values[3].render} \nB #{@cells.values[4].render} #{@cells.values[5].render} #{@cells.values[6].render} #{@cells.values[7].render} \nC #{@cells.values[8].render} #{@cells.values[9].render} #{@cells.values[10].render} #{@cells.values[11].render} \nD #{@cells.values[12].render} #{@cells.values[13].render} #{@cells.values[14].render} #{@cells.values[15].render} \n"
   end
 end
