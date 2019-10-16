803530af5b0e147630b6aa6af719159d4df94cee
Paul Debevec <paulmdebevec@gmail.com>
2019-10-16 16:26:09 -0600

    Created game initial setup
    Co-Authored-By: Phil DeLong <philjdelong@gmail.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,44 +1,77 @@
-require'minitest/autorun'
-require'minitest/pride'
-require'./lib/ship'
-require'./lib/cell'
-require'./lib/board'
-require'pry'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/computer'
+require 'pry'
 
-@computer_board = Board.new
-@player_board = Board.new
-@cruiser = Ship.new("Cruiser", 3)
-@computer_cruiser = Ship.new("Cruiser", 3)
-@submarine = Ship.new("Submarine", 2)
-@computer_submarine = Ship.new("Submarine", 2)
+def startup
 
-puts "Welcome to BATTLESHIP"
-puts "Enter p to play. Enter q to quit."
+  # @computer = Computer.new
+  # @computer_board = Board.new
+  # @player_board = Board.new
+  @cruiser = Ship.new("Cruiser", 3)
+  # @computer_cruiser = Ship.new("Cruiser", 3)
+  @submarine = Ship.new("Submarine", 2)
+  # @computer_submarine = Ship.new("Submarine", 2)
+  # @user_input = gets.chomp
+
+  def board_setup
+    puts "=============COMPUTER BOARD============="
+    computer_board = Board.new
+    computer = Computer.new(computer_board)
+    computer_cruiser = Ship.new("Cruiser", 3)
+    computer_submarine = Ship.new("Submarine", 2)
+    computer.generate_placement(computer_cruiser)
+    computer.generate_placement(computer_submarine)
+    puts computer.board.render
+
+    puts "=============PLAYER BOARD============="
+    puts "*Computer*: I have laid out my ships on the board."
+    player_board = Board.new
+    puts player_board.render
+    player_cruiser = Ship.new("Cruiser", 3)
+    player_submarine = Ship.new("Submarine", 2)
+    puts "*Computer*: Now enter the coordinates to place your #{player_cruiser.name}"
+
+    # binding.pry
+    player_input = gets.chomp.split(" ")
+    place_ship = player_board.place(player_cruiser, player_input)
+
+    # player_input.each do |coord|
+    #   coord.valid_coordinate?
+    #   # puts "Invalid coordinate"
+    #   end
+    # end
 
 
+    puts "=============COMPUTER BOARD============="
+    puts computer.board.render
+    puts "=============PLAYER BOARD============="
+    puts player_board.render(true)
+    # binding.pry
 
-puts "=============COMPUTER BOARD============="
-puts @computer_board.render
+  end
 
-puts "=============PLAYER BOARD============="
-puts @player_board.render(true)
+  # def take_turn
+    # until @cruiser.sunk? && @submarine.sunk? || @computer_cruiser.sunk? && @computer_submarine.sunk?
+  #
+  #   end
+  # end
+  #
+  def main_menu
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    user_input = gets.chomp
+    if user_input == "p" || "P"
+      board_setup
+    elsif user_input == "q" || "Q"
+      puts "You suck"
+    else
+      "That's not what we asked for"
+    end
+  end
 
-# this method should initiate the compoter-generated placement
+  main_menu
+end
 
-p "I have laid out my ships on the grid."
-p "You now need to lay out your two ships."
-p "The Cruiser is three units long and the Submarine is two units long."
-p "
-1 2 3 4
-A . . . .
-B . . . .
-C . . . .
-D . . . .
-"
-p "Enter the squares for the Cruiser (3 spaces):"
-  coords = gets.chomp
-  @board.place_ship(@cruiser, coords)
-
-p @board.render(true)
-
-"Enter the squares for the Submarine (2 spaces):"
+startup
