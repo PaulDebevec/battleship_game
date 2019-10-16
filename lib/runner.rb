91c2af72fde4e07e6f893defa1d82628c6cb826f
Paul Debevec <paulmdebevec@gmail.com>
2019-10-16 17:50:52 -0600

    Created working runner up to fire_upon
    Co-Authored-By: Phil DeLong <philjdelong@gmail.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -15,7 +15,20 @@ def startup
   # @computer_submarine = Ship.new("Submarine", 2)
   # @user_input = gets.chomp
 
-  def board_setup
+  def main_menu
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    user_input = gets.chomp
+    if user_input == "p" || "P"
+      start_game
+    elsif user_input == "q" || "Q"
+      puts "You suck"
+    else
+      puts "That's not what we asked for"
+    end
+  end
+
+  def start_game
     puts "=============COMPUTER BOARD============="
     computer_board = Board.new
     computer = Computer.new(computer_board)
@@ -34,44 +47,40 @@ def startup
     puts "*Computer*: Now enter the coordinates to place your #{player_cruiser.name}"
 
     # binding.pry
-    player_input = gets.chomp.split(" ")
+    player_input = gets.chomp.upcase.split(" ")
     place_ship = player_board.place(player_cruiser, player_input)
-
-    # player_input.each do |coord|
-    #   coord.valid_coordinate?
-    #   # puts "Invalid coordinate"
-    #   end
-    # end
-
 
     puts "=============COMPUTER BOARD============="
     puts computer.board.render
     puts "=============PLAYER BOARD============="
     puts player_board.render(true)
-    # binding.pry
 
-  end
+    puts "*Computer*: Now, place your #{player_submarine.name}"
+    player_input = gets.chomp.upcase.split(" ")
+    place_ship = player_board.place(player_submarine, player_input)
 
-  # def take_turn
-    # until @cruiser.sunk? && @submarine.sunk? || @computer_cruiser.sunk? && @computer_submarine.sunk?
-  #
-  #   end
-  # end
-  #
-  def main_menu
-    puts "Welcome to BATTLESHIP"
-    puts "Enter p to play. Enter q to quit."
-    user_input = gets.chomp
-    if user_input == "p" || "P"
-      board_setup
-    elsif user_input == "q" || "Q"
-      puts "You suck"
-    else
-      "That's not what we asked for"
+    puts "=============COMPUTER BOARD============="
+    puts computer.board.render
+    puts "=============PLAYER BOARD============="
+    puts player_board.render(true)
+    # ********* Add a guard clause for invalid placements and off-board placements *********
+    # if player_board.ship
+    until player_cruiser.sunk? && player_submarine.sunk? || computer_cruiser.sunk? && computer_submarine.sunk?
+      puts "Enter a coordinate to fire upon"
+      # binding.pry
+      player_shot = gets.chomp.upcase
+      computer_board.cells[player_shot].fire_upon
+      # player_board.cells.random_fire_upon
+      puts "=============COMPUTER BOARD============="
+      puts computer.board.render
+      puts "=============PLAYER BOARD============="
+      puts player_board.render(true)
+
     end
-  end
+    end
 
   main_menu
+
 end
 
 startup
