29231bf79c0e42867bbece8ee35501d3691a1088
Phil DeLong <philjdelong@gmail.com>
2019-10-15 19:18:09 -0600

    Write generate_placement method to generate cells and validate cell placement

--- lib/computer.rb
+++ lib/computer.rb
@@ -5,6 +5,14 @@ class Computer
     @board = board
   end
 
-  
+  def generate_placement(ship)
+    loop do
+      coordinates = @board.cells.keys.each_cons(ship.length).to_a.sample
+      if @board.valid_placement?(ship, coordinates) && @board.no_ship_in_cell(coordinates)
+        @board.place(ship, coordinates)
+        break
+      end
+    end
+  end
 
 end
